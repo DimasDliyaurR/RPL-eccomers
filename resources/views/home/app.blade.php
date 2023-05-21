f059bd0cd238ba3b2f00753bcb3b3942141a3d56
= <dimasdliyaurrahman@gmail.com>
2023-05-21 09:24:06 +0700

    first commit

---
+++ resources/views/home/app.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Furniture</title>
+</head>
+
+<body>
+    {{-- Header --}}
+    <div class="header">
+
+        <div class="logo">
+            logo
+        </div>
+
+        <div class="navigasi">
+            <ul>
+                <li>Categories</li>
+                <li>Brand</li>
+                <li>Pesanan</li>
+            </ul>
+        </div>
+
+        <div class="search">
+            <input type="text" placeholder="Search product">
+        </div>
+
+        <div class="item">
+            <ul>
+                <li>Shop</li>
+                <li>Profil</li>
+            </ul>
+        </div>
+    </div>
+
+    {{-- End Header --}}
+
+    <div class="thumbnail">
+        <h2>Gambar</h2>
+    </div>
+
+    <div class="contant">
+
+        <div class="card">
+            <div class="card-thumbnail">
+    
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
