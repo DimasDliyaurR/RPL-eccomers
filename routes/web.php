f176167200a50655aca4acba0e30cf1ab0dac039
= <dimasdliyaurrahman@gmail.com>
2023-06-01 17:04:22 +0700

    login access :  admin , user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\appController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\appController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +16,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('login');
+});
 
-Route::controller(appController::class)->group(function (){
+Route::controller(appController::class)->group(function(){
     Route::get('/','index');
 });
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('admin')->group(function () {
+    
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+});
+require __DIR__.'/auth.php';
